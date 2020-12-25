8b80d75e78004305950ff6fe2f2e9fc22c79a7f5
Chandradhar Cherukuvada <chandradhar93@pm.me>
2020-12-26 01:45:03 +0530

    Dec 25 Update Added

---
+++ LearnMM/LearnMM- Dec25/categories/categories.php
@@ -0,0 +1,63 @@
+<html>
+
+<head>
+	
+</head>
+
+<body>
+	<div class= container>
+		<p> Choose a category to begin with: </p>
+		<div class ="row">
+			<!--Card-->
+			<div class = "col-lg border p-4 m-4 text-lg-center bg-primary ">
+				<a href="http://learn.misplacedminds.com/search.php?search=unity">
+				<p></p>
+				<p class="text-white">Unity</p>
+				<p></p></a>
+			</div>
+			<!--Card-->
+			<div class = " col-lg border p-4 m-4 text-md-center bg-info">
+				<a href="http://learn.misplacedminds.com/search.php?search=HTML">
+				<p></p>
+				<p class="text-dark">FrontEnd</p>
+				<p></p></a>
+			</div>
+			<!--Card-->
+			<div class = " col-lg border p-4 m-4 text-md-center bg-warning">
+				<a href="http://learn.misplacedminds.com/search.php?search=php">
+				<p></p>
+				<p class="text-dark">PHP</p>
+				<p></p></a>
+			</div>
+			<div class="w-100"></div>
+			<!--Card-->
+			<div class = " col-lg border p-4 m-4 text-md-center bg-success">
+				<a href="http://learn.misplacedminds.com/tutorials.php">
+				<p></p>
+				<p class="text-white">Programming</p>
+				<p></p></a>
+			</div>
+			<!--Card-->
+			<div class = " col-lg border p-4 m-4 text-md-center bg-danger">
+				<a href="http://learn.misplacedminds.com/search.php?search=design">
+				<p></p>
+				<p class="text-white">Design Patterns</p>
+				<p></p></a>
+			</div>
+			<!--Card-->
+			<div class = " col-lg border p-4 m-4 text-md-center bg-dark">
+				<a href="articles.php">
+				<p></p>
+				<p class="text-white">Articles</p>
+				<p></p></a>
+			</div>
+			
+			
+			
+
+		</div>
+	</div>
+
+</body>
+
+</html>
